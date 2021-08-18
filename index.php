46582e08aab3c15d052c431ac53e3ae54baa9822
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2021-08-18 20:55:34 +0800

    Register

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "social");
+
+if (mysqli_connect_errno()){
+  echo "Failed to connect: " . mysqli_connect_error();
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Twitter Clone</title>
+</head>
+<body>
+
+</body>
+</html>
