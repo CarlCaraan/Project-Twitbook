413a2bdbf96ed6467ba5b41e2d89095b4a23b673
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2021-08-19 21:10:27 +0800

    Register

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "social");
+require 'config/config.php';
 
-if (mysqli_connect_errno()){
-  echo "Failed to connect: " . mysqli_connect_error();
-}
 ?>
-
 
 
 <!DOCTYPE html>
