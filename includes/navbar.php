f07d88e9eb349207977eb9f90da10c5825506aa8
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2021-08-22 19:49:19 +0800

    landing page

--- includes/navbar.php
+++ includes/navbar.php
@@ -21,35 +21,59 @@ else {
 
 	<div class="collapse navbar-collapse" id="navbarResponsive">
 		<ul class="navbar-nav mx-auto">
-			<li class="nav-item">
+			<li class="nav-item text-center mx-auto">
 				<a class="nav-link" href="index.php">
 					<i class="fas fa-home fa-inverse <?php if($page=='home'){echo 'active';}?>" data-fa-transform="grow-10"></i>
                 </a>
+
+                <div class="underline-spacing"></div>
+                <div class=" <?php if($page=='home'){echo 'heading-underline';}?>"></div>
+
 			</li>
-			<li class="nav-item">
+			<li class="nav-item text-center mx-auto">
 				<a class="nav-link" href="#">
 					<i class="fas fa-envelope fa-inverse <?php if($page=='messages'){echo 'active';}?>" data-fa-transform="grow-10"></i>
                 </a>
+
+                <div class="underline-spacing"></div>
+                <div class=" <?php if($page=='#'){echo 'heading-underline';}?>"></div>
+
 			</li>
-			<li class="nav-item">
+			<li class="nav-item text-center mx-auto">
 				<a class="nav-link" href="#">
 					<i class="fas fa-globe-americas fa-inverse <?php if($page=='notification'){echo 'active';}?>" data-fa-transform="grow-10"></i>
                 </a>
+
+                <div class="underline-spacing"></div>
+                <div class=" <?php if($page=='#'){echo 'heading-underline';}?>"></div>
+
 			</li>
-			<li class="nav-item">
+			<li class="nav-item text-center mx-auto">
 				<a class="nav-link" href="#">
 					<i class="fas fa-users fa-inverse <?php if($page=='friendrequest'){echo 'active';}?>" data-fa-transform="grow-10"></i>
                 </a>
+
+                <div class="underline-spacing"></div>
+                <div class=" <?php if($page=='#'){echo 'heading-underline';}?>"></div>
+
 			</li>
-			<li class="nav-item">
+			<li class="nav-item text-center mx-auto">
 				<a class="nav-link" href="settings.php">
 					<i class="fas fa-cog fa-inverse <?php if($page=='settings'){echo 'active';}?>" data-fa-transform="grow-10"></i>
                 </a>
+
+                <div class="underline-spacing"></div>
+                <div class=" <?php if($page=='settings'){echo 'heading-underline';}?>"></div>
+
 			</li>
-			<li class="nav-item">
+			<li class="nav-item text-center mx-auto">
 				<a class="nav-link" href="#">
 					<i class="fas fa-sign-out-alt fa-inverse <?php if($page=='logout'){echo 'active';}?>" data-fa-transform="grow-10"></i>
                 </a>
+
+                <div class="underline-spacing"></div>
+                <div class=" <?php if($page=='#'){echo 'heading-underline';}?>"></div>
+
 			</li>
 		</ul>
 	</div>
