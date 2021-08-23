ff2a1288e83af2b0e93151cf2afd6ee71bed94f4
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2021-08-23 11:48:00 +0800

    8/23/21

--- includes/head.php
+++ includes/head.php
@@ -5,5 +5,6 @@
 	<link rel="stylesheet" href="bootstrap-4.6.0-dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="assets/css/fixed.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"> <!--- For animation.css CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"> <!-- For animation.css CDN -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> <!-- FIX REGISTER SHOW/HIDE -->
