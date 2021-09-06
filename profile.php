10d2b5755ad928766eda9e8562b3af0c41d8f6d7
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2021-09-06 15:21:28 +0800

    9/6/21

--- profile.php
+++ profile.php
@@ -23,6 +23,7 @@ if(isset($_GET['profile_username'])) {
 	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
 	$user_array = mysqli_fetch_array($user_details_query);
 
+	$num_friends = (substr_count($user_array['friend_array'], ",")) -1;
 }
 ?>
 
@@ -57,15 +58,33 @@ if(isset($_GET['profile_username'])) {
 	<!-- Start Section Content -->
 	<section>
 		<article>
-			<div class="narrow">
-				<h1>
+			<div class="container">
+
+			<div class="row">
+
+				<!-- Start Intro Column -->
+				<div class="col-lg-4" id="col-wrapper">
+					<label id="post_text">Intro</label><br>
+
+					<div id="icon_wrapper"><i class="fas fa-file-alt"></i></div>
+					<div id="text_wrapper"><?php echo "Posts: " . $user_array['num_posts']; ?></div><br>
+
+					<div id="icon_wrapper"><i class="fas fa-flag"></i></div>
+					<div id="text_wrapper"><?php echo "Likes: " . $user_array['num_likes']; ?></div><br>
+
+					<div id="icon_wrapper"><i class="fas fa-user-friends"></i></div>
+					<div id="text_wrapper"><?php echo "Friends: " . $num_friends; ?></div>
+
+				</div>
+				<!-- End Intro Column -->
+
+				<div class="col-lg-7" id="col-wrapper">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</h1>
-			</div>
+				</div>
+
+			</div> <!-- End Row -->
+
+		</div> <!-- End Narrow -->
 		</article>
 	</section>
 	<!-- End Section Content -->
